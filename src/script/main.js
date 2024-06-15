c680d0e7c3860c7977ab607519022e453fa147e2
044developer <044developer@gmail.com>
2024-06-15 17:41:18 +0300

    F! base html, css and js structure
    - filled login and form with html
    - added styles
    - added main script to handle all click events

---
+++ src/script/main.js
@@ -0,0 +1,65 @@
+const container = document.querySelector(".login-container");
+const loginLinkButton = document.querySelector(".login-link");
+const registerLinkButton = document.querySelector(".register-link");
+const navBarLoginButton = document.querySelector(".nav-login-button");
+const closePopupButton = document.querySelector(".icon-close");
+const homeButton = document.querySelector(".nav-home");
+const aboutButton = document.querySelector(".nav-about");
+const servicesButton = document.querySelector(".nav-services");
+const contactButton = document.querySelector(".nav-contact");
+
+const clickEventName = "click";
+const popupTriggerKey = "active-popup";
+const registrationTriggerKey = "registration-active";
+
+function onRegisterButtonClicked() {
+  container.classList.add(registrationTriggerKey);
+}
+
+function onLoginButtonClicked() {
+  container.classList.remove(registrationTriggerKey);
+}
+
+function onNavBarLoginButtonClicked() {
+  container.classList.add(popupTriggerKey);
+}
+
+function onPopupCloseButtonClick() {
+  container.classList.remove(popupTriggerKey);
+}
+
+function notImplementedFeatureAllert() {
+  alert("Feature is not implemented, yet");
+}
+
+function onHomeButtonClick() {
+  notImplementedFeatureAllert();
+}
+
+function onAboutButtonClick() {
+  notImplementedFeatureAllert();
+}
+
+function onServicesButtonClick() {
+  notImplementedFeatureAllert();
+}
+
+function onContactButtonClick() {
+  notImplementedFeatureAllert();
+}
+
+function SubscribeEvents() {
+  registerLinkButton.addEventListener(clickEventName, onRegisterButtonClicked);
+  loginLinkButton.addEventListener(clickEventName, onLoginButtonClicked);
+  navBarLoginButton.addEventListener(
+    clickEventName,
+    onNavBarLoginButtonClicked
+  );
+  closePopupButton.addEventListener(clickEventName, onPopupCloseButtonClick);
+  homeButton.addEventListener(clickEventName, onHomeButtonClick);
+  aboutButton.addEventListener(clickEventName, onAboutButtonClick);
+  servicesButton.addEventListener(clickEventName, onServicesButtonClick);
+  contactButton.addEventListener(clickEventName, onContactButtonClick);
+}
+
+SubscribeEvents();
